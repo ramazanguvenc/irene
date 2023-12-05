223f67de6513eee8d93fe065669f8a2dcce873b5
root <root@server2390.isimkayit.com>
2023-12-05 21:58:09 +0000

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify the name of the screen session to kill
+SCREEN_NAME="IreneBot"
+
+# Find the session ID for the specified screen name
+SESSION_ID=$(screen -ls | grep "$SCREEN_NAME" | awk '{print $1}' | cut -d'.' -f1)
+
+# Check if the session ID is not empty (screen session found)
+if [ -n "$SESSION_ID" ]; then
+    # Send the kill command to the screen session
+    screen -S "$SESSION_ID" -X quit
+    echo "Screen session $SCREEN_NAME killed successfully."
+else
+    echo "No screen session found with the name $SCREEN_NAME."
+fi
+
+# Run Maven commands
+mvn clean && mvn install && screen -dmS IreneBot bash -c "mvn exec:java"
+
